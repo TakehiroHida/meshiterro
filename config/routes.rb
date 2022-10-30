01fa245c03e5472fd7f9b72a25ab82e6f6c0c7a1
EC2 Default User <ec2-user@ip-172-31-47-39.ap-northeast-1.compute.internal>
2022-10-30 07:00:03 +0000

    (add)meshiterro_3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
+  
+  get '/homes/about', as: 'about'
 end
